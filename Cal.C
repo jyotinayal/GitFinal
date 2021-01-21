20cca90d8db4885d21944e9c406bf5daa4b29b0c
jyoti nayal <jyoti.nayal@cuelogic.com>
2021-01-21 17:00:55 +0530

    mistake correction

--- Cal.C
+++ Cal.C
@@ -1,17 +1,11 @@
 #include <stdio.h>
 void add(int a,int b)
 {
-    int c=a+b;
-    if(c<0)
-    {c=c*-1;}
-    printf("%d",c);
+    printf("%d",  a + b);
 }
 void sub(int a,int b)
 {
-    int c=a-b;
-    if(c<0)
-    {c=c*-1;}
-    printf("%d",c);
+    printf("%d",  a + b);
 }
 int main()
 {
